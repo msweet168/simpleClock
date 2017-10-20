7fb770d40351e62a8e1ae9eec8343865325d6e65
msweet168 <mitchellsweet168@aol.com>
2017-10-19 23:03:37 -0400

    Simple Clock is now Simple World Clock. User can now choose one of four locations. Background image will download from URL to set theme of clock. Clock will change to selected time zone. Added struct to store location, Added image loader to download image from url. Added info view controller for app information and references.

--- simpleClock/ViewController.swift
+++ simpleClock/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
+/// Stores attributes of supported locations.
+struct Location {
+    let displayName: String
+    let url: URL?
+    let timeZone: TimeZone
+}
+
 class ViewController: UIViewController {
     
-    // Create outlets for UIViews acting as hands on clock.
+    // Create outlets for UIViews acting as hands on clock and time selector segmented view.
     @IBOutlet var hourHand: UIView!
     @IBOutlet var minuteHand: UIView!
+    @IBOutlet var timeSelector: UISegmentedControl!
+    @IBOutlet var backgroundImageView: UIImageView!
     
-    // Create timer to update time each second.
-    var timeUpdater: Timer?
-    // Create constant for calendar.
-    let calendar = Calendar.current
+    // Array to store location structs for supported locations.
+    static let supportedLocations = [
+        Location(displayName: "Current Location",
+                 url: nil,
+                 timeZone: Calendar.current.timeZone),
+        Location(displayName: "Cupertino",
+                 url: URL(string: "https://i.imgur.com/3EsGLo9.jpg"),
+                 timeZone: TimeZone(abbreviation: "PST")!),
+        Location(displayName: "London",
+                 url: URL(string: "https://i.imgur.com/zO2CMlT.jpg"),
+                 timeZone: TimeZone(abbreviation: "UTC")!),
+        Location(displayName: "Florence",
+                 url: URL(string: "https://i.imgur.com/ydRPSGh.jpg"),
+                 timeZone: TimeZone(abbreviation: "CET")!)
+    ]
     
-
+    var timeUpdater: Timer? // Create timer to update time each second.
+    let imageLoader = ImageLoader()
+    var selectedLocation: Location { // Variable for current location, will update each time used.
+        return ViewController.supportedLocations[timeSelector.selectedSegmentIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,29 +52,41 @@ class ViewController: UIViewController {
         hourHand.layer.cornerRadius = 2
         minuteHand.layer.cornerRadius = 2
         
-        update() // Call the update function.
+        update()
         
         // Initalize timer to call the update function once every second.
-        timeUpdater = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
-        //FIXME: Find a more efficient way to do this.
+        timeUpdater = Timer.scheduledTimer(timeInterval: 1,
+                                           target: self,
+                                           selector: #selector(update),
+                                           userInfo: nil,
+                                           repeats: true)
+    }
+
+    /// Called when new location is selected in segmented control.
+    @IBAction func didChangeLocation() {
+        if let locationURL = selectedLocation.url {
+            imageLoader.loadImage(url: locationURL) { image, error in
+                if let error = error {
+                    print("Error: Could not download image. Description: \(error.localizedDescription)")
+                }
+                self.backgroundImageView.image = image
+            }
+        }
+        else {
+            backgroundImageView.image = nil // Set background image to nil if user chooses current time zone.
+        }
+        update()
     }
     
     /// Gets the current date and updates the hands on the clock based on the time.
     @objc func update() {
-        let date = Date()
-        updateMinuteHand(minute: calendar.component(.minute, from: date))// Call function to update minute hand with current minute.
+        let components = Calendar.current.dateComponents(in: selectedLocation.timeZone,
+                                                         from: Date())
+        updateMinuteHand(minute: components.minute!)
+        updateHourHand(hour: components.hour! % 12 , minute: components.minute!)
         
-        // Date formatter to get current hour in 12 hour format.
-        let formatter = DateFormatter()
-        formatter.dateFormat = "hh"
-        let hourString = formatter.string(from: date)
-        if let hourInt = Int(hourString) {
-            updateHourHand(hour: hourInt, minute: calendar.component(.minute, from: date)) // Call function to update hour hand with current hour.
-        }
-        else {
-            fatalError("Was not able to update hour hand, no int value in date.")
-        }
     }
+    
     
     /// Rotates the hour hand x amount of radians based on the current time.
     func updateHourHand(hour: Int, minute: Int) {
@@ -85,13 +122,12 @@ class ViewController: UIViewController {
         view.layer.anchorPoint = anchorPoint;
     }
     // https://stackoverflow.com/questions/26815263/setting-a-rotation-point-for-cgaffinetransformmakerotation-swift
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
